58551a4c80a8995dc138f4a66f6909d16d063f50
PheakdeyHov <hovpheakdey463@gmail.com>
2025-01-19 13:42:52 +0700

    success

--- database/migrations/2025_01_18_071855_create_sales_table.php
+++ database/migrations/2025_01_18_071855_create_sales_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->string('invoice');
+            $table->decimal('total_price',10,2);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2025_01_19_034046_create_sale_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sale_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('specification_id')->constrained()->onDelete('cascade');
+            $table->integer('qty');
+            $table->decimal('sale_price',10,2);
+            $table->enum('status',['new','second-hand','99']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sale_products');
+    }
+};
